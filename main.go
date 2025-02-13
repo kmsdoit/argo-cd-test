a3abc088e467208527c532af020a8cfeaf057dee
kmsdoit <klmmms882912@naver.com>
2025-02-13 15:52:10 +0900

    v3

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
 	r := gin.Default()
-
+	log.Println("test")
 	r.PUT("/v1/:key", KeyValuePutHandler)
 	r.GET("/v1/:key", KeyValueGetHandler)
 	r.DELETE("/v1/:key", KeyValueDeleteHandler)
